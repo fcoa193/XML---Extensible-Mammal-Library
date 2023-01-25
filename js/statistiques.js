03904b802af1328615f695f21d129cbcfb616186
EvilCorpRobot <theo.conte38250@gmail.com>
2023-01-25 16:23:35 +0100

    adding cart

--- js/statistiques.js
+++ js/statistiques.js
@@ -1,13 +1,31 @@
 import {getXmlAnimals} from "./animals.js"
 
 const addChart = (nameChart) => {
-
+    const chart = document.querySelector("#chart")
+    getXmlAnimals((xmlDoc) => {
+        let weight = xmlDoc.getElementsByTagName("weight")
+        console.log(weight)
+        for (let item of weight) {
+            const name = item.parentNode.getElementsByTagName("name")[0].innerHTML
+            chart.appendChild(addProgressBar(name, item.innerHTML))
+        }
+    })
 }
 
 const addProgressBar = (label, value) => {
+    let animalChart = document.createElement("div")
+    animalChart.classList.add("animal-chart")
+    let p = document.createElement("p")
+    p.innerHTML = label
     const progress = document.createElement("div")
-    progressBar.classList.add("progress")
+    progress.classList.add("progress")
     const progressBar = document.createElement("div")
     progressBar.classList.add("progress-bar")
     progressBar.style = "width: " + value + "%;"
+    progress.appendChild(progressBar)
+    animalChart.appendChild(p)
+    animalChart.appendChild(progress)
+    return animalChart
 }
+
+addChart()
