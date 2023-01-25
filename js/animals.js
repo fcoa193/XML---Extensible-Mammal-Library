a1f4261b6a01dd5cfd0ec81367f5aebba357d33f
EvilCorpRobot <theo.conte38250@gmail.com>
2023-01-25 15:46:06 +0100

    js added

---
+++ js/animals.js
@@ -0,0 +1,10 @@
+export const getXmlAnimals = (callback) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "../animal.xml", true)
+  xhr.send()
+  xhr.onreadystatechange = async () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      if (callback) callback(xhr.responseXML)
+    }
+  }
+}
---
+++ js/statistiques.js
@@ -0,0 +1,13 @@
+import {getXmlAnimals} from "./animals.js"
+
+const addChart = (nameChart) => {
+
+}
+
+const addProgressBar = (label, value) => {
+    const progress = document.createElement("div")
+    progressBar.classList.add("progress")
+    const progressBar = document.createElement("div")
+    progressBar.classList.add("progress-bar")
+    progressBar.style = "width: " + value + "%;"
+}
